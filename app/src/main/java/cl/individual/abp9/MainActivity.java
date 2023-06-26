71fcf96a36e87688c4df96fc16d45fb3f69db4ce
Emilia <correoalcantaraemilia@gmail.com>
2023-06-26 19:00:06 -0400

    Update diseño inicial de la main activity + se crea una segunda actividad + se agrega funcionalidad a cada imageButton (al presionar se pasa a la segunda actividad).

--- app/src/main/java/cl/individual/abp9/MainActivity.java
+++ app/src/main/java/cl/individual/abp9/MainActivity.java
@@ -2,7 +2,10 @@ package cl.individual.abp9;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
+    }
+
+    private void initListeners() {
+        ImageButton imgBoton1 = findViewById(R.id.imgBoton1);
+        ImageButton imgBoton2 = findViewById(R.id.imgBoton2);
+        ImageButton imgBoton3 = findViewById(R.id.imgBoton3);
+        ImageButton imgBoton4 = findViewById(R.id.imgBoton4);
+
+        imgBoton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity();
+            }
+        });
+
+        imgBoton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity();
+            }
+        });
+
+        imgBoton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity();
+            }
+        });
+
+        imgBoton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToSecondActivity();
+            }
+        });
+
+    }
+
+    public void moveToSecondActivity() {
+        Intent moveToSecondAct = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(moveToSecondAct);
     }
 }
