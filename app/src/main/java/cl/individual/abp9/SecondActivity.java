52eb8e5a6b4fbf8b24a7dd062cbbba85321ffcf5
Emilia <correoalcantaraemilia@gmail.com>
2023-06-26 19:12:32 -0400

    Boton volver atrás funcional (desde la segunda actividad a la primera).

--- app/src/main/java/cl/individual/abp9/SecondActivity.java
+++ app/src/main/java/cl/individual/abp9/SecondActivity.java
@@ -3,6 +3,8 @@ package cl.individual.abp9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button botonVolverAtras = findViewById(R.id.botonVolver);
+
+        botonVolverAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
